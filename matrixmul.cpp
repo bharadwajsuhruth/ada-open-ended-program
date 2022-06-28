7d6c1f613a23adaeca5540adbec934c3f403827c
Bharadwaj Suhruth <108287375+bharadwajsuhruth@users.noreply.github.com>
2022-06-28 19:09:41 +0530

    Update matrixmul.cpp

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void Mat(int d[],int n) //Function that calculates the minimum chain multiplication and finds the equation
+void Mat(int d[],int n) 
 {
   int a[n][n],b[n],min,count=0;
   bool var[n];
@@ -9,17 +9,17 @@ void Mat(int d[],int n) //Function that calculates the minimum chain multiplicat
 
   for(int i=0;i<n;i++)
   {
-    var[i]=false; //Sets all values in the array to false to prevent garbage values
+    var[i]=false;
 
     for(int j=0;j<n-i;j++)
     {
-      a[j][j+i]=9999; //Sets the initial matrix cell to a large integer to get replaced by the calculated value on first run
+      a[j][j+i]=9999; 
 
       for(int k=j;k<j+i;k++)
       {
-        min=a[j][k]+a[k+1][j+i]+(d[j]*d[k+1]*d[j+i+1]); //Finds the value for respective K value using the formula
+        min=a[j][k]+a[k+1][j+i]+(d[j]*d[k+1]*d[j+i+1]); 
 
-        if(min<a[j][j+i]) //Replaces the stored value in matrix cell with a minimum value
+        if(min<a[j][j+i])
         {
           a[j][j+i]=min;
           if(j==0)
@@ -27,17 +27,17 @@ void Mat(int d[],int n) //Function that calculates the minimum chain multiplicat
         }
       }
 
-      if(i==0) //Used to set all diagonal matrix cells to 0
+      if(i==0) 
         a[j][j+i]=0;
     }
   }
 
-  for(int i=n-1;i>0;i--) //Sets the index of variables that needs to be separated by a bracket to TRUE
+  for(int i=n-1;i>0;i--) 
   {
     var[b[i]+1]=true;
   }
 
-  for(int i=1;i<n;i++) //Adds open paranthesis to the necessary variables
+  for(int i=1;i<n;i++) 
   {
     if(var[i])
     {
@@ -48,12 +48,12 @@ void Mat(int d[],int n) //Function that calculates the minimum chain multiplicat
       eq+="a"+to_string(i);
   }
 
-  for(int i=0;i<count;i++) //Closes all the open paranthesis
+  for(int i=0;i<count;i++) 
   {
     eq="("+eq;
   }
 
-  eq+="a"+to_string(n)+")"; //Final equation string
+  eq+="a"+to_string(n)+")"; 
 
   cout<<endl<<"Minimum Multiplications: "<<a[0][n-1]<<endl;
   cout<<"The equation with minimum multiplications is: "<<eq<<endl;
@@ -69,7 +69,7 @@ int main()
 
   cout<<"Enter the size of the matrices in order\n";
 
-  for(int i=0;i<n;i++) //Takes in the size for all the matrices
+  for(int i=0;i<n;i++) 
   {
     cout<<"Matrix "<<i+1<<endl;
     cout<<"Row: ";
@@ -78,7 +78,7 @@ int main()
     cin>>a[i][1];
   }
 
-  for(int i=0;i<=n;i++) //Creates an array containing d0 to dN values to be used in the main formula
+  for(int i=0;i<=n;i++) 
   {
     if(i!=n)
       d[i]=a[i][0];
